02e60b421229feb68321633983a8952134d813e7
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-02-01 15:59:47 +0100

    ajout errorTest

---
+++ judoClub/src/test/java/fr/iut/tp/servlets/ErrorTest.java
@@ -0,0 +1,31 @@
+package fr.iut.tp.servlets;
+
+import junit.framework.TestCase;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebResponse;
+
+public class ErrorTest extends TestCase {
+	private WebConversation webConversation = new WebConversation();
+	private static final String URL_ROOT = "http://localhost:8080/judoClub";
+	
+	public ErrorTest(final String s) {
+		super(s);
+	}
+	
+	
+	/**
+	 * Test avec un mot de passe invalide 
+	 */
+	public void testBadLogin() throws Exception {
+		WebResponse page;
+		page = webConversation.getResponse(URL_ROOT + "/action/qsfsdqfq");
+		WebResponse newPage = webConversation.getCurrentPage();
+		String title = newPage.getTitle();
+		assertEquals("la page doit etre une error 404", "404", title);
+	}
+	
+
+	
+}
